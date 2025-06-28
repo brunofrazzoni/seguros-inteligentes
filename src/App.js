b008ac12f73d06b7e2fef31a3ce38bd095792004
brunofrazzoni <bruno.frazzoni13@gmail.com>
2025-06-28 12:41:56 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InsuranceApp from './components/InsuranceApp';
+
+function App() {
+  return (
+    <div className="App">
+      <InsuranceApp />
+    </div>
+  );
+}
+
+export default App;
